5f05b6f9d087ab66913d4f0ffead1be40e076738
Eury Sosa Garcia <112292926+watercubz@users.noreply.github.com>
2024-01-09 17:50:21 -0400

    fix: error types

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 import { CustomSpinner } from "./core.js";
 
 // elige el stylo de tu Snipify que deseas (puedes cambiar 'dots' por otro estilo)
-const Spinify = new CustomSpinner("line", "loading...");
+const Spinify = new CustomSpinner("lines", "loading...");
 Spinify.start();
 
 // simula una tarea que lleva tiempo
