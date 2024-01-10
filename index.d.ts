4dc562db5d027f8e46e3765f07a2436cc87194d9
watercubz <eurysosagarcia@gmail.com>
2024-01-10 18:59:08 -0400

    fix: type errors

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,23 @@
 export declare class CustomSpinner {
+     /**
+   * Constructor for CustomSpinner class.
+   * @param style - The style of the spinner.
+   * @param text - The text to display with the spinner.
+   */
     constructor(style?: string, text?: string);
+
     start(): void;
+    /**
+   * @example
+   * ```typescript
+   * import { CustomSpinner } from 'spinify';
+   * const spetSpinner = new CustomSpinner("spet", "Loading...");
+   * spetSpinner.start();
+   * setTimeout(() => {
+   *   spetSpinner.stop();
+   * }, 5000);
+   * ```
+   */
     stop(): void;
 
 }
